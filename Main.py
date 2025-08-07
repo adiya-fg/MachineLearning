6ddaa4d185f5244544c743d889f12b0aceaed460
Adiya <adiya.tb@gmail.com>
2025-08-07 13:56:13 +0900

    adding more functions into main engine

---
+++ Main.py
@@ -0,0 +1,6 @@
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(
+iris_dataset['data'], iris_dataset['target'], random_state=0)
+
+print("X_train shape: {}".format(X_train.shape))
+print("y_train shape: {}".format(y_train.shape))
